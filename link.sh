9b5aa8e8199fd6bc99056a14db21be9acb3ec1f8
jason <jason@Unknown.gateway>
2018-06-06 12:55:21 +1000

    a

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dotfiles=(".vimrc" ".profile" ".gitconfig" ".fzf.bash" ".inputrc")
+dotfiles=(".vimrc" ".profile" ".gitconfig" ".fzf.bash" ".inputrc" "xterm-256color-italic.terminfo")
 dir="${HOME}/dotfiles"
 for dotfile in "${dotfiles[@]}";do
  ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
