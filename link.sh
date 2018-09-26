aa1aafabc8ff6a50e064055548366e22f0662953
jason <jason@MacBook.gateway>
2018-09-26 20:39:03 +1000

    removing not dot files

--- link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dotfiles=(".vimrc" ".profile" ".gitconfig" ".fzf.bash" ".inputrc" "xterm-256color-italic.terminfo")
-dir="${HOME}/dotfiles"
-for dotfile in "${dotfiles[@]}";do
- ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
-done
